1f4cbad1d5da00630edbb3436d2c923935e19ea3
ginterrupt <gigalfc@gmail.com>
2026-02-24 22:31:51 +0400

    sync ok

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
-# Register your models here.
-=======
 from .models import Snippet
 
 # Register your models here.
 
 admin.site.register(Snippet)
->>>>>>> temp-fix
 
 #done
